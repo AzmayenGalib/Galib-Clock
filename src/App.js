24c5a4d1edc4e82c643172c790f6196576871072
AzmayenGalib <98214380+AzmayenGalib@users.noreply.github.com>
2022-02-10 18:57:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable eqeqeq */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let date = new Date();
+  let time = new Date().toLocaleTimeString();
+  const [ctime, settime] = useState(time);
+  let upsec = date.getSeconds();
+  let bar = date.getDay();
+  let month = date.getMonth();
+  let dat = date.getDate();
+  let year = date.getFullYear();
+
+  setInterval(() => {
+    let time = new Date().toLocaleTimeString();
+    settime(time);
+  }, 1000);
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June ",
+    "July",
+    "Augest",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "thursday",
+    "Friday",
+    "Saturday",
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="color"></div>
+      <div className="color"></div>
+      <div className="color"></div>
+      <h1 className="head zori">Galib Clock</h1>
+      <div className="contain">
+        <p className="zori">
+          {day[bar]}
+          <br></br>
+          {dat} {months[month]}
+          <br></br>
+          {year}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="zori">{ctime}</h1>
+      </div>
     </div>
   );
 }
